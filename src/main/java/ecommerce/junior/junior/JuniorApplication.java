fe17a2a76f2dbd32ae051d071eab5f6cb337630d
samuhkls <samuel.henriquess21@gmail.com>
2024-08-29 20:58:55 -0300

    commit inicial

---
+++ src/main/java/ecommerce/junior/junior/JuniorApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.junior.junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce/junior/junior/JuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.junior.junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
